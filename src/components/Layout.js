71658ccf8afd594234e02a86d017f523e0fbfdb3
YashAgarwal1201 <legoyashx@gmail.com>
2023-01-19 00:40:46 +0530

    commit 1 19-01-2023

---
+++ src/components/Layout.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Navbar from '../components/Navbar.js'
+
+export default function Layout() {
+	return (
+		<div>
+			
+		</div>
+	)
+}
--- src/pages/404.js
+++ src/pages/404.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function NotFound() {
 	return (
-		<div className="w3-panel">
+		<div className="w3-panel w3-center">
 			<h2>404</h2>
 			<p>Sorry but page is not found</p>
 		</div>
